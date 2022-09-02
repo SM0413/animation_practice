3073e9ac43d7c2022501459b68ab18de6534ae59
SM0413 <kbjtmdals@naver.com>
2022-09-02 10:56:02 +0900

    MotionValues part Three

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import styled from "styled-components";
-import { motion, useMotionValue, useTransform } from "framer-motion";
-import { useEffect } from "react";
+import { motion, useMotionValue, useTransform, useScroll } from "framer-motion";
 
-const Wrapper = styled.div`
-  height: 100vh;
+const Wrapper = styled(motion.div)`
+  height: 200vh;
   width: 100vw;
   display: flex;
   justify-content: center;
@@ -21,13 +20,21 @@ const Box = styled(motion.div)`
 
 function App() {
   const x = useMotionValue(0);
-  const potato = useTransform(x, [-700, 0, 700], [2, 1, 0.1]);
-  useEffect(() => {
-    potato.onChange(() => console.log(potato.get()));
-  });
+  const rotateY = useTransform(x, [-700, 700], [-360, 360]);
+  const gradient = useTransform(
+    x,
+    [-700, 0, 700],
+    [
+      "linear-gradient(135deg, hsl(196.1344537815126, 100%, 46.666666666666664%), hsl(257.89915966386553, 100%, 46.666666666666664%))",
+      "linear-gradient(135deg, hsl(321.42857142857144, 100%, 46.666666666666664%), hsl(295.7142857142857, 100%, 46.666666666666664%))",
+      "linear-gradient(135deg, hsl(132.10084033613447, 100%, 46.666666666666664%), hsl(72.10084033613445, 100%, 46.666666666666664%))",
+    ]
+  );
+  const { scrollYProgress } = useScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
   return (
-    <Wrapper>
-      <Box style={{ x, scale: potato }} drag="x" dragSnapToOrigin />
+    <Wrapper style={{ background: gradient }}>
+      <Box style={{ x, rotateY, scale }} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
 }
